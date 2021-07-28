34ed4144fb0a5dbadcd22e4eca355ab1cdc2ac6b
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-07-28 23:59:58 +0530

    embed

--- src/App.js
+++ src/App.js
@@ -2,5 +2,9 @@ import "./styles.css";
 import ReactDOM from "react-dom";
 
 export default function App() {
-  ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+  const name = "Basil Maben";
+  const element = <h1>Hello, {name}</h1>;
+
+  ReactDOM.render(element, document.getElementById("root"));
+  return null;
 }
