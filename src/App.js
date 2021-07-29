f700d57f05d3bce833f927a8fcddfc6b65ef7745
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-07-29 23:58:53 +0530

    jsx

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import "./styles.css";
 import ReactDOM from "react-dom";
 
 export default function App() {
-  const name = "Basil Maben";
-  const element = <h1>Hello, {name}</h1>;
+  function formatName(user) {
+    return user.firstName + " " + user.lastName;
+  }
+
+  const user = {
+    firstName: " Basil",
+    lastName: "Maben"
+  };
+
+  const element = <h1>Hello, {formatName(user)}!</h1>;
 
   ReactDOM.render(element, document.getElementById("root"));
-  return null;
 }
