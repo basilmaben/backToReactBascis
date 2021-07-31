6d53728956697922476de2df1bb12a04cddae8d7
basilmaben <70912563+basilmaben@users.noreply.github.com>
2021-07-31 23:58:10 +0530

    components and props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./styles.css";
 import ReactDOM from "react-dom";
 
-export default function App() {
-  function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-    ReactDOM.render(element, document.getElementById("root"));
-  }
-
-  setInterval(tick, 5000);
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
